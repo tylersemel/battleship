84f988089739ccd505798dee595f03467a5ea0a0
tylersemel <tylersemel@gmail.com>
2025-09-25 14:24:29 -0400

    Update eslint jest config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,12 +4,18 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
+    files: ["**/*.{js,mjs,cjs}", "**/*.test.js"],
+    plugins: { js, jest: pluginJest },
     extends: ["js/recommended"],
   },
   {
     files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    languageOptions: { 
+      globals: { ...globals.browser, ...globals.node, ...globals.jest }, 
+      rules: {
+        ...pluginJest.configs.recommended.rules
+      } 
+    },
   },
+  
 ]);
