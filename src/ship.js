6205d51df1b4de75338ba55115d66659b602d2ad
tylersemel <tylersemel@gmail.com>
2025-09-26 11:56:47 -0400

    Test that a ship can be hit and sunk

--- src/ship.js
+++ src/ship.js
@@ -1,17 +1,25 @@
 class Ship {
     constructor(length) {
-        if (this.checkValidLength(length)) {
+        if (this.#checkValidLength(length)) {
             this.length = length;
         } else {
             throw new Error("Length needs to be between 1 and 4 units.");
         }
         
-        this.hit = 0;
+        this.hits= 0;
         this.sunk = false;
     }
 
-    checkValidLength(length) {
+    #checkValidLength(length) {
         return !(length <= 0 || length > 4);
+    }
+
+    hit() {
+        this.hits++;
+    }
+
+    isSunk() {
+        return this.hits >= this.length;
     }
 }
 
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,14 +1,40 @@
 import { Ship } from "./ship";
 
 test('Create a ship with a valid length', () => {
-    expect(new Ship(1)).toEqual({length: 1, hit: 0, sunk: false});
+    expect(new Ship(1)).toEqual({length: 1, hits: 0, sunk: false});
 });
 
 test('Create a ship with an invalid length', () => {
     expect(() => new Ship(0)).toThrow();
+    expect(() => new Ship(5)).toThrow();
+    expect(() => new Ship(4)).not.toThrow();
 });
 
-lengths: [1, 2, 3, 4];
+test('Hit a ship once', () => {
+    const ship = new Ship(2);
+    expect(ship).toEqual({length: 2, hits: 0, sunk: false});
+    ship.hit()
+    expect(ship).toEqual({length: 2, hits: 1, sunk: false});
+});
 
+test('Check if a 1 length ship is sunk', () => {
+    const ship = new Ship(1);
 
-const ship = { length: 1, hit: 2, sunk: false };
+    expect(ship.isSunk()).toBe(false);
+
+    ship.hit();
+
+    expect(ship.isSunk()).toBe(true);
+});
+
+test('Check if a 3 length ship is sunk', () => {
+    const ship = new Ship(3);
+
+    expect(ship.isSunk()).toBe(false);
+
+    ship.hit();
+    ship.hit();
+    ship.hit();
+
+    expect(ship.isSunk()).toBe(true);
+});
